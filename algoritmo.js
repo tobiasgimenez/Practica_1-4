823f038859f39a9a947120fd284fdcec8366e76f
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-07 11:09:40 -0300

    Termine la practica 1-4

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.write(`
+<details>
+    <summary> EN EL ESTE Y EL OESTE
+EN EL NORTE Y EN EL SUR</summary>
+    <p> <TItle="texto">
+        BRILLARA LA BLANCA Y CELESTE LA ACADEMIA RACING CLUB
+    </p>
+    <img title="personas claves para el campeonato" src="https://www.elprimergrande.com/wp-content/uploads/2019/03/racing-campeon-gracias-e1554081292687.png" alt="racing campeón">
+    <BR></BR>
+    <a href="https://www.youtube.com/watch?v=CxzPwCbyuxY&t=1s" title="camino de racing hacia el campeonato">RACING CAMPEÓN 2019</a>
+</details>
+`)
